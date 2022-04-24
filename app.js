2f6ffade1e254e974c25a2adf14b3e6165f9ad09
Mott Laulha <mottlaulha@gmail.com>
2022-04-24 21:19:15 +0100

    Le menu du mobile est fait. Il reste cependant deux truc à faire pour que tout soit complet.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
+const menuElement = document.querySelector('.header__nav__menu');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        menuElement.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open')
+        menuElement.classList.remove('open')
         menuOpen = false;
     }
 });
