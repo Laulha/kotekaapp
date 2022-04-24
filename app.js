546eb21cac2424ffd7a27b2ed5aff794908ca398
Mott Laulha <mottlaulha@gmail.com>
2022-04-24 21:38:50 +0100

    Overlay à été créé avec succès. Il reste un seul truc pour finir tout ça.

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuElement = document.querySelector('.header__nav__menu');
+const overlay_mobile = document.querySelector('.overlay-menu-mobile');
 let menuOpen = false;
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menuElement.classList.add('open');
+        overlay_mobile.classList.add('open');
         menuOpen = true;
     } else {
-        menuBtn.classList.remove('open')
-        menuElement.classList.remove('open')
+        menuBtn.classList.remove('open');
+        menuElement.classList.remove('open');
+        overlay_mobile.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+overlay_mobile.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuElement.classList.add('open');
+        overlay_mobile.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuElement.classList.remove('open');
+        overlay_mobile.classList.remove('open');
         menuOpen = false;
     }
 });
