725e6298072fef0721564b7774365f821c71cc46
Mott Laulha <mottlaulha@gmail.com>
2022-04-24 21:56:26 +0100

    Le bouton de téléchargement à été géré avec succès.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuElement = document.querySelector('.header__nav__menu');
 const overlay_mobile = document.querySelector('.overlay-menu-mobile');
+const btn_telechargement = document.querySelector('.telecharger');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
@@ -8,11 +9,13 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.add('open');
         menuElement.classList.add('open');
         overlay_mobile.classList.add('open');
+        btn_telechargement.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menuElement.classList.remove('open');
         overlay_mobile.classList.remove('open');
+        btn_telechargement.classList.remove('open');
         menuOpen = false;
     }
 });
@@ -22,11 +25,13 @@ overlay_mobile.addEventListener('click', () => {
         menuBtn.classList.add('open');
         menuElement.classList.add('open');
         overlay_mobile.classList.add('open');
+        btn_telechargement.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menuElement.classList.remove('open');
         overlay_mobile.classList.remove('open');
+        btn_telechargement.classList.remove('open');
         menuOpen = false;
     }
 });
