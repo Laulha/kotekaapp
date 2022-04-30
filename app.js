3b876bcb3f50d769cb05bd2da28d3addcf642a81
Mott Laulha <mottlaulha@gmail.com>
2022-04-30 13:40:05 +0100

    Mes cards on été fait succès.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const menuBtn = document.querySelector('.menu-btn');
 const menuElement = document.querySelector('.header__nav__menu');
 const overlay_mobile = document.querySelector('.overlay-menu-mobile');
 const btn_telechargement = document.querySelector('.telecharger');
+const btnSelectPays = document.querySelector('.licence__plan__select-box');
+const paysAffiche = document.querySelector('.licence__plan__select-box__liste');
 let menuOpen = false;
+let selectPays = false;
 
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
@@ -34,4 +37,44 @@ overlay_mobile.addEventListener('click', () => {
         btn_telechargement.classList.remove('open');
         menuOpen = false;
     }
+});
+
+btnSelectPays.addEventListener('click', () => {
+    if (!selectPays) {
+        paysAffiche.classList.add('clique');
+        selectPays = true;
+    } else {
+        paysAffiche.classList.remove('clique');
+        selectPays = false;
+    }
+});
+
+const elementListeSelcection = document.querySelectorAll('.licence__plan__select-box__liste div');
+const titrePays = document.querySelector('.licence__plan__select-box__titre span');
+
+elementListeSelcection.forEach(element => {
+    
+    element.addEventListener('click', () => {
+        
+        let pays = element.innerHTML;
+        titrePays.innerHTML = pays;
+
+        switch (pays) {
+            case 'Algérie':
+                
+                break;
+
+            case 'Congo RC':
+            
+                break;
+
+            case 'Cameroun':
+        
+                break;
+        
+            default:
+                break;
+        }
+
+    })
 });
